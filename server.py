84ac0b4cdc9edd5b7d7cf28e5389e095b4651b30
evoandroidevo <github@evoandroidevo.me>
2023-01-22 04:29:22 +0000

    test

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import requests
 import hmac
 import hashlib
 import subprocess
+import datetime
 
 app = Flask(__name__)
 
@@ -22,7 +23,7 @@ def verify(api_key, body, signature):
     #     print("hmac digest is string", file=sys.stdout)
     return hmac.compare_digest(fsignature, hmac_digest.encode('utf-8'))
 
-def _get_header(key):
+def getHeader(key):
     """Return message header"""
 
     try:
@@ -30,41 +31,48 @@ def _get_header(key):
     except KeyError:
         abort(400, "Missing header: " + key)
 
-def threaded(func):
-    #https://stackoverflow.com/questions/67071870/python-make-a-function-always-use-a-thread-without-calling-thread-start
-    """Decorator to automatically launch a function in a thread"""
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):  # replaces original function...
-        # ...and launches the original in a thread
-        thread = threading.Thread(target=func, args=args, kwargs=kwargs)
-        print("Starting Thread")
-        thread.start()
-        return thread
-    return wrapper
+def pullGit(path):
+    # process = subprocess.Popen(["git", "pull", "-C", path], stdout=subprocess.PIPE)
+    # output = process.communicate()[0]
+    sttime = datetime.now().strftime('%Y%m%d_%H:%M:%S - ')
+    f = open("test.log", "a")
+    f.write(sttime + path + '\n')
+    f.close()
+    return
 
-def deploy():
-    
-    return None
+# def threaded(func):
+#     #https://stackoverflow.com/questions/67071870/python-make-a-function-always-use-a-thread-without-calling-thread-start
+#     """Decorator to automatically launch a function in a thread"""
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):  # replaces original function...
+#         # ...and launches the original in a thread
+#         thread = threading.Thread(target=func, args=args, kwargs=kwargs)
+#         print("Starting Thread")
+#         thread.start()
+#         return thread
+#     return wrapper
+
+def deploy(gitName, private):
+    if not private:
+        pullGit(gitName)
+        return 'success', 201
+    else:
+        return 'Erorr', 400
 
 @app.route('/deploy', methods=['POST'])
 def webhook():
     """Webhook POST listener"""
     if request.method == 'POST':
         if "GitHub-Hookshot" in _get_header("User-Agent"):
-            if verify(secret, request.data, _get_header("X-Hub-Signature-256")):
-                #deploy()
-                return 'success', 201
+            if verify(secret, request.data, getHeader("X-Hub-Signature-256")):
+                repo = request.json.get('repository')
+                return deploy(repo.get('name'), repo.get('private'))
             else:
                 abort(401)
         else:
             abort(418)
     else:
         abort(405)
-
-@app.route('/')
-def results():
-    content = "nothing"
-    return '<html><head>custom head stuff here</head><body><br>' + content + '</body></html>'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
