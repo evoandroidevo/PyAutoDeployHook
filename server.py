aa614a392426372e46afc4c0e67c20cc188ce1af
evoandroidevo <github@evoandroidevo.me>
2023-02-08 01:47:06 +0000

    Working webhook posting

--- server.py
+++ server.py
@@ -19,9 +19,9 @@ def getURL(repoName):
     """Switch statment for webhook urls"""
     match repoName:
         case "":
-            return ""
+            return "https://webhook.site/1ba19da1-0195-47ee-97b9-e30f48302177"
         case _:
-            return ""
+            return "https://webhook.site/1ba19da1-0195-47ee-97b9-e30f48302177"
 
 
 def verify(api_key, body, signature):
@@ -51,31 +51,34 @@ def have_internet() -> bool: #https://stackoverflow.com/questions/3764291/how-ca
     finally:
         conn.close()
 
-def threaded(func):
-    #https://stackoverflow.com/questions/67071870/python-make-a-function-always-use-a-thread-without-calling-thread-start
-    """Decorator to automatically launch a function in a thread"""
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):  # replaces original function...
-        # ...and launches the original in a thread
-        thread = threading.Thread(target=func, args=args, kwargs=kwargs)
-        print("Starting Thread")
-        thread.start()
-        return thread
-    return wrapper
+# def threaded(func):
+#     #https://stackoverflow.com/questions/67071870/python-make-a-function-always-use-a-thread-without-calling-thread-start
+#     """Decorator to automatically launch a function in a thread"""
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):  # replaces original function...
+#         # ...and launches the original in a thread
+#         thread = threading.Thread(target=func, args=args, kwargs=kwargs)
+#         print("Starting Thread")
+#         thread.start()
+#         return thread
+#     return wrapper
 
-@threaded
-def sendWebhook(arg):
+#@threaded
+@retry(NoInternet, delay=5, tries=30, backoff=30, max_delay=120)
+def sendWebhook(arg, sendURL):
     #used https://gist.github.com/Bilka2/5dd2ca2b6e9f3573e0c2defe5d3031b2
     #as a base for this.
     try:
         if have_internet():
-            result = requests.post(sendurl, json=arg)
+            result = requests.post(sendURL, json=arg)
             if 200 <= result.status_code < 300:
-                print(f"Webhook sent. \nReturned Code: {result.status_code}")
-                return result.status_code
+                #print(f"Webhook sent. \nReturned Code: {result.status_code}")
+                returnData = f"Webhook sent. \nReturned Code: {result.status_code}"
+                return returnData, result.status_code
             else:
-                print(f"Not sent with {result.status_code}, response:\n{result.json()}")
-                return result.status_code
+                #print(f"Not sent with {result.status_code}, response:\n{result.json()}")
+                errorData = f"Not sent with {result.status_code}, response:\n{result.json()}"
+                return errorData, result.status_code
         else:
             raise NoInternet("ERROR: No Internet Detected.")
         
@@ -100,11 +103,12 @@ def deploy(gitName, private, data):
     """Deploy logic"""
     match gitName:
         case "PyAutoDeployHook":
-            pullGit(gitName)
+            #pullGit(gitName)
+            return sendWebhook(data, getURL(gitName))
         case "authelia":
-            sendWebhook(data, geturl(gitName))
+            return sendWebhook(data, getURL(gitName))
         case _:
-            return
+            return "No info", 400
     # if not private:
     #     return pullGit(gitName)
     # else:
@@ -130,5 +134,5 @@ def webhook():
 
 if __name__ == '__main__':
     logging.info('Starting')
-    app.run()
+    app.run(host="0.0.0.0")
     logging.info('Exiting')
